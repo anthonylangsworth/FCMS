82a75dc9ae82bb68f1f4fc14ea670c0ccac75914
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-19 17:16:29 +1100

    Add text box for FCMS CMDR name

--- load.py
+++ load.py
@@ -3,6 +3,8 @@ import requests
 import logging
 import tkinter as tk
 import os
+from typing import Tuple, Optional, Dict, Any
+import json
 
 import myNotebook as nb
 from config import config, appname
@@ -16,31 +18,30 @@ this.version_info = (0, 2, 0)
 this.version = ".".join(map(str, this.version_info))
 this.api_url = "https://fleetcarrier.space/api"
 
+CONFIG_EMAIL = "fcms_emails"
+
 # Setup logging
 logger = logging.getLogger(f'{appname}.{os.path.basename(os.path.dirname(__file__))}')
 
 
-def plugin_start(plugin_dir):
+def plugin_start(plugin_dir:str) -> str:
     # Migrate from single setting
     if not config.get("fcms_cmdrs"):
         if config.get("FCMSEmail"):
             # first commander will get the old settings
             config.set("fcms_emails", [config.get("FCMSEmail") or ""])
             config.set("fcms_apikeys", [config.get("FCMSKey") or ""])
+            config.set("fcms_cmdr_names", [config.get("FCMSKey") or ""])
         config.delete("FCMSEmail")
         config.delete("FCMSKey")
     return this.plugin_name
 
 
-def plugin_start3(plugin_dir):
+def plugin_start3(plugin_dir:str) -> str:
     return plugin_start(plugin_dir)
 
 
-def plugin_app(parent):
-    return None
-
-
-def plugin_prefs(parent, cmdr, is_beta):
+def plugin_prefs(parent:nb.Notebook, cmdr: str, is_beta:bool) -> Optional[tk.Frame]:
 
     PADX = 10
     PADY = 2
@@ -66,8 +67,8 @@ def plugin_prefs(parent, cmdr, is_beta):
     ).grid(row=1, columnspan=2, padx=PADX, sticky=tk.W)
 
     nb.Label(this.cred_frame, text=_("Cmdr")).grid(row=10, padx=PADX, sticky=tk.W)
-    this.cmdr_text = nb.Label(this.cred_frame)
-    this.cmdr_text.grid(row=10, column=1, padx=PADX, pady=PADY, sticky=tk.W)
+    this.cmdr_text = nb.Entry(this.cred_frame)
+    this.cmdr_text.grid(row=10, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
 
     nb.Label(this.cred_frame, text=_("Email")).grid(row=11, padx=PADX, sticky=tk.W)
     this.email = nb.Entry(this.cred_frame)
@@ -82,7 +83,7 @@ def plugin_prefs(parent, cmdr, is_beta):
     return frame
 
 
-def set_state_frame_childs(frame, state):
+def set_state_frame_childs(frame:tk.Frame, state):
     for child in frame.winfo_children():
         if child.winfo_class() in ("TFrame", "Frame", "Labelframe"):
             set_state_frame_childs(child, state)
@@ -90,7 +91,7 @@ def set_state_frame_childs(frame, state):
             child["state"] = state
 
 
-def credentials(cmdr):
+def get_credentials(cmdr:str) -> Tuple[str, str, str]:
     # Credentials for cmdr
     if cmdr:
         cmdrs = config.get("fcms_cmdrs")
@@ -98,73 +99,83 @@ def credentials(cmdr):
             # Migrate from single setting, first commander gets the old settings
             cmdrs = [cmdr]
             config.set("fcms_cmdrs", cmdrs)
-        emails = config.get("fcms_emails")
-        apikeys = config.get("fcms_apikeys")
+        if not cmdr in cmdrs:
+            cmdrs.append(cmdr)
+        emails = config.get("fcms_emails") or [""] * len(cmdrs)
+        apikeys = config.get("fcms_apikeys") or [""] * len(cmdrs)
+        cmdr_names = config.get("fcms_cmdr_names") or [""] * len(emails)
         if cmdr in cmdrs and emails and apikeys:
             idx = cmdrs.index(cmdr)
-            return (emails[idx], apikeys[idx])
+            if not cmdr_names[idx]:
+                cmdr_names[idx] = cmdr
+            return (cmdr_names[idx], emails[idx], apikeys[idx])
     return None
 
 
-def prefs_cmdr_changed(cmdr, is_beta):
+def prefs_cmdr_changed(cmdr:str, is_beta:bool) -> None:
     set_state_frame_childs(this.cred_frame, tk.NORMAL)
+    this.cmdr_text.delete(0, tk.END)
     this.email.delete(0, tk.END)
     this.apikey.delete(0, tk.END)
     if cmdr:
-        this.cmdr_text["text"] = cmdr + (is_beta and " [Beta]" or "")
-        cred = credentials(cmdr)
+        cred = get_credentials(cmdr)
         if cred:
-            this.email.insert(0, cred[0])
-            this.apikey.insert(0, cred[1])
+            this.cmdr_text.insert(0, cred[0])
+            this.email.insert(0, cred[1])
+            this.apikey.insert(0, cred[2])
     else:
-        this.cmdr_text["text"] = _("None")
+        this.cmdr_text.insert(0, _("None"))
 
     if not cmdr or is_beta:
         set_state_frame_childs(this.cred_frame, tk.DISABLED)
 
+# Preferences are saved in the registry at "HKEY_CURRENT_USER\SOFTWARE\Marginal\EDMarketConnector"
 
-def prefs_changed(cmdr, is_beta):
+def prefs_changed(cmdr:str, is_beta: bool) -> None:
     if cmdr and not is_beta:
         cmdrs = config.get("fcms_cmdrs")
-        emails = config.get("fcms_emails") or []
-        apikeys = config.get("fcms_apikeys") or []
+        emails = config.get("fcms_emails") or [""] * len(cmdrs)
+        apikeys = config.get("fcms_apikeys") or [""] * len(cmdrs)
+        cmdr_names = config.get("fcms_cmdr_names") or [""] * len(cmdrs)
         if cmdr in cmdrs:
             idx = cmdrs.index(cmdr)
-            emails.extend([""] * (1 + idx - len(emails)))
             emails[idx] = this.email.get().strip()
-            apikeys.extend([""] * (1 + idx - len(apikeys)))
             apikeys[idx] = this.apikey.get().strip()
+            cmdr_names[idx] = this.cmdr_text.get().strip()
         else:
             config.set("fcms_cmdrs", cmdrs + [cmdr])
             emails.append(this.email.get().strip())
             apikeys.append(this.apikey.get().strip())
+            cmdr_names.append(this.cmdr_text.get().strip())
         config.set("fcms_emails", emails)
         config.set("fcms_apikeys", apikeys)
+        config.set("fcms_cmdr_names", cmdr_names)
 
 
 # See https://github.com/FuelRats/FCMS/blob/master/FCMS/views/api.py for server side of call.
 # See https://www.w3schools.com/python/ref_requests_response.asp for response help
 
-def journal_entry(cmdr, is_beta, system, station, entry, state):
+def journal_entry(cmdr:str, is_beta:bool, system:Optional[str], station:Optional[str], entry:Dict[str, Any], stateentry:Dict[str, Any]) -> Optional[str]:
+    result = None
     if entry["event"] in ["CarrierJumpRequest", "CarrierJumpCancelled"] and not is_beta:
-        cred = credentials(cmdr)
+        cred = get_credentials(cmdr)
         if cred:
             post = {
-                "cmdr": cmdr, # "CMDR " + cmdr,
+                "cmdr": cred[0],
                 "system": system,
                 "station": station,
                 "data": entry,
                 "is_beta": is_beta,
-                "user": cred[0],
-                "key": cred[1],
+                "user": cred[1],
+                "key": cred[2],
             }
             response = requests.post(this.api_url, json=post)
             if response.status_code == 200:
                 logger.info(f"{ entry['event']} event posted to FCMS")
             else:
                 logger.info(f"{ entry['event']} event posting to FCMS failed: { str(response.status_code) }")
-                # TODO: Consider prepending CMDR and retrying
+                result = f"{this.plugin_name}: Error updating FCMS. Check CMDR name."
         else:
             logger.error("No credentials")
-    elif entry["event"] in ("LoadGame", "NewCommander") and not credentials(cmdr):
-        logger.error("No credentials")
+            result = f"{this.plugin_name}: Add credentials."
+    return result
