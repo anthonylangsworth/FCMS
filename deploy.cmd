1e1b32ba895bfebefd986d7579c753a3912e7243
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-27 16:44:47 +1100

    Add error handling

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,13 @@
 set dest=%userprofile%\AppData\Local\EDMarketConnector\plugins\FCMS
-rd /s /q %dest%
-mkdir %dest%
-xcopy /y load.py %dest%
+rd /s /q %dest% || goto error
+mkdir %dest% || goto error
+xcopy /y load.py %dest% || goto error
+xcopy /y fcms_web_services.py %dest% || goto error
+
+goto end
+
+:error
+echo Error %errorlevel%
+exit /b %errorlevel%
+
+:end
