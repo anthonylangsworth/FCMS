487b6ec71d66868e533e2615aee0f1e5d37f3af1
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-27 21:17:18 +1100

    Start adding EDMC mocks

---
+++ edmc_mocks.py
@@ -0,0 +1,28 @@
+# Mock EDMC-provided functionality to:
+# 1. Stop `flake8` flagging errors.
+# 2. Allow unit testing.
+
+from typing import Any
+
+try:
+    _("a")
+except NameError:
+    def _(s):
+        return s
+
+try:
+    config.get()
+except NameError:
+    class Config():
+        def __init__(self):
+            self._dict = dict()
+        
+        def get(self, key:str) -> Any:
+            return self._dict.get(key, None)
+
+        def set(self, key:str, value:Any) -> None:
+            self._dict[key] = value
+
+        def delete(self, key:str) -> None:
+            del self._dict[key]
+    config = Config()
--- load.py
+++ load.py
@@ -5,15 +5,12 @@ import tkinter as tk
 import os
 from typing import Tuple, Optional, Dict, Any, Union
 
-import myNotebook as nb
-from config import config, appname
-from ttkHyperlinkLabel import HyperlinkLabel
-
 try:
-    _("a")
-except NameError:
-    def _(s):
-        return s
+    import myNotebook as nb
+    from config import config, appname
+    from ttkHyperlinkLabel import HyperlinkLabel
+except ImportError:
+    from edmc_mocks import *
 
 import fcms_web_services
 
