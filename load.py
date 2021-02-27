2737bc1c040ab6269ae0ea94f679877a512107a6
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-27 22:11:42 +1100

    Increment version

--- load.py
+++ load.py
@@ -18,7 +18,7 @@ this = sys.modules[__name__]
 this.plugin_name = "FCMS"
 this.plugin_url = "https://github.com/anthonylangsworth/FCMS"
 this.apikey_url = "https://fleetcarrier.space/my_carrier"
-this.version_info = (0, 6, 0)
+this.version_info = (0, 7, 0)
 this.version = ".".join(map(str, this.version_info))
 this.api_url = "https://fleetcarrier.space/api"
 
